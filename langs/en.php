858acfabaf56dcc4afb037bd41eaf325368b6685
Nikolai Romashchenko <kolya.romashenko@yandex.ru>
2019-05-03 22:25:25 +0300

    popular-russians [PHP]

---
+++ langs/en.php
@@ -0,0 +1,20 @@
+<?php
+	$en = array(
+		'pageTitle' => "Popular Russian people",
+		'aboutSite' => "About this site",
+		'close' => 'Close',
+		'modalLabelLomonosov' => "Mikhail Lomonosov",
+	    'modalLomonosov' => "
+							<p><strong>Mikhail Vasilyevich Lomonosov</strong> (/ˌlɒməˈnɒsɒf/;[1] Russian: Михаи́л (Михáйло) Васи́льевич Ломоно́сов, IPA: [mʲɪxɐˈil vɐˈsʲilʲjɪvʲɪtɕ ləmɐˈnosəf] (About this soundlisten); November 19 [O.S. November 8] 1711 – April 15 [O.S. April 4] 1765) was a Russian polymath, scientist and writer, who made important contributions to literature, education, and science. Among his discoveries were the atmosphere of Venus and the law of conservation of mass in chemical reactions. His spheres of science were natural science, chemistry, physics, mineralogy, history, art, philology, optical devices and others. Lomonosov was also a poet and influenced the formation of the modern Russian literary language.
+							</p>
+							<p>Lomonosov was born in the village of Mishaninskaya (later renamed Lomonosovo in his honor) in Archangelgorod Governorate, on an island not far from Kholmogory, in the far north of Russia.[2] His father, Vasily Dorofeyevich Lomonosov, was a prosperous peasant fisherman turned ship owner, who amassed a small fortune transporting goods from Arkhangelsk to Pustozyorsk, Solovki, Kola, and Lapland. Lomonosov's mother was Vasily's first wife, a deacon's daughter, Elena Ivanovna Sivkova.
+							</p>
+							<p>He remained at Denisovka until he was ten, when his father decided that he was old enough to participate in his business ventures, and Lomonosov began accompanying Vasily on trading missions.
+							</p>
+							<p>Learning was young Lomonosov's passion, however, not business. The boy's thirst for knowledge was insatiable. Lomonosov had been taught to read as a boy by his neighbor Ivan Shubny, and he spent every spare moment with his books. He continued his studies with the village deacon, S.N. Sabelnikov, but for many years the only books he had access to were religious texts. When he was fourteen, Lomonosov was given copies of Meletius Smotrytsky's Modern Church Slavonic (a grammar book) and Leonty Magnitsky's Arithmetic. Lomonosov was a Russian orthodox all his life, but had close encounters with Old Believers schism in early youth and later in life he became a deist.</p>
+							<p>In 1724, his father married for the third and final time. Lomonosov and his stepmother Irina had an acrimonious relationship. Unhappy at home and intent on obtaining a higher education, which Lomonosov could not receive in Mishaninskaya, he was determined to leave the village.</p>
+	    ",
+		'modalLabelErmak' => "",
+	    'modalErmak' => "",
+	);
+?>
